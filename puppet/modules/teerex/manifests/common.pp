268edac130827ccbb13e535f80e49bf8db3ace08
hotelca <quoc.honza@gmail.com>
2020-09-16 00:18:49 -0700

    Updated app location to use C:\ProgramData instead

--- puppet/modules/teerex/manifests/common.pp
+++ puppet/modules/teerex/manifests/common.pp
@@ -1,21 +1,44 @@
 class teerex::common {
-  $home_dir = $facts['windows_env']['USERPROFILE']
-  $suite_dir = "${home_dir}/Genie Suite"
+  $home_dir = $facts['windows_env']['ALLUSERSPROFILE']
+  $binary_dir = "${home_dir}\\teerex-puppet"
+  $suite_dir = "${home_dir}\\Genie Suite"
+  $program_files_dir = "C:\\Program Files"
+  $puppet_dir = "${program_files_dir}\\Puppet Labs\\Puppet\\bin"
+  $git_dir = 'C:\\Program Files\\Git\\bin'
 
+  exec { 'update_checkout':
+    path    => $git_dir,
+    cwd     => $binary_dir,
+    command => 'git pull',
+  } ->
   file { "${suite_dir}":
     ensure => 'directory',
   } ->
-  file { "${suite_dir}/logs":
+  file { "${suite_dir}\\logs":
     ensure => 'directory',
   } ->
-  file { "${suite_dir}/Designs":
+  file { "${suite_dir}\\Designs":
     ensure => 'directory',
   } ->
-  file { "${suite_dir}/Designs_ARX4":
+  file { "${suite_dir}\\Designs_ARX4":
     ensure => 'directory',
   } ->
-  exec { 'run_sciprt':
-    command => "${home_dir}/teerex-puppet/Design_Loader/Design_Loader.exe",
+  exec { 'run_script':
+    command => "${binary_dir}\\Design_Loader\\Design_Loader.exe",
+  }
+
+  scheduled_task { 'run_design_loader_hourly':
+    ensure    => present,
+    enabled   => true,
+    working_dir  => $puppet_dir,
+    command   => "${puppet_dir}\\puppet.bat",
+    arguments => "apply --modulepath=${binary_dir}\\puppet\\modules;${home_dir}\\.puppetlabs\\etc\\code\\modules --logdest=\"${suite_dir}\\logs\\puppet_logs.txt\" ${binary_dir}\\puppet\\manifests\\site.pp",
+    trigger   => {
+      schedule         => daily,
+      start_time       => '00:00',
+      minutes_interval => '5',
+      minutes_duration => '1440',
+    }
   }
 }
 
